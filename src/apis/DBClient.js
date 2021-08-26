81029583752c35dcdb8c1b67fe934f696761b848
Per Eriksson <per@interapp.se>
2021-08-26 23:40:03 +0200

    added route for single document

--- src/apis/DBClient.js
+++ src/apis/DBClient.js
@@ -1,5 +1,6 @@
 const {DATABASE_HOSTNAME, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE, DATABASE_COLLECTION} = require("../constants.js");
 const {MongoClient} = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 
 class DBClient {
     async getDocuments() {
@@ -13,13 +14,34 @@ class DBClient {
 
         const query = {};
 
-        const cursor = documents.find(query);
+        const cursor = await documents.find(query);
 
         const result = await cursor.toArray();
 
         await client.close();
 
         return result;
+    }
+
+    async getDocument(id) {
+        const url = `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOSTNAME}`;
+
+        const client = new MongoClient(url);
+
+        await client.connect();
+        const db = client.db(DATABASE_DATABASE);
+        const documents = db.collection(DATABASE_COLLECTION);
+
+        const _id = new ObjectId(id);
+        const query = {_id};
+
+        const cursor = await documents.find(query);
+
+        const result = await cursor.toArray();
+
+        await client.close();
+
+        return result[0];
     }
 }
 
--- src/v1/index.js
+++ src/v1/index.js
@@ -11,4 +11,12 @@ router.get("/documents", async (req, res) => {
     res.json(documents);
 });
 
+router.get("/documents/:id", async(req, res) => {
+    const client = new DBClient();
+
+    const document = await client.getDocument(req.params.id);
+
+    res.json(document);
+});
+
 module.exports = router;
