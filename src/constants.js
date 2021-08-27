57d4b8ab1738a35bd5f3e3309db1a93750a1f4b6
Per Eriksson <per@interapp.se>
2021-08-27 13:49:36 +0200

    added ability to configure the app

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,12 @@
 // Database
-const DATABASE_HOSTNAME = "cluster0.zp46i.mongodb.net";
-const DATABASE_USER = "user";
-const DATABASE_PASSWORD = "38IjSUubHI0TE4Ij";
-const DATABASE_DATABASE = "editor";
-const DATABASE_COLLECTION = "documents";
+const DATABASE_HOSTNAME = process.env.DATABASE_HOSTNAME || "cluster0.zp46i.mongodb.net";
+const DATABASE_USER = process.env.DATABASE_USER || "user";
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "38IjSUubHI0TE4Ij";
+const DATABASE_DATABASE = process.env.DATABASE_DATABASE || "editor";
+const DATABASE_COLLECTION = process.env.DATABASE_COLLECTION || "documents";
 
 // Express
-const PORT = 1337;
+const PORT = process.env.PORT || 1337;
 
 module.exports = {
     DATABASE_HOSTNAME,
