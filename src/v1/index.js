befa760139064755140c933c96ba4ddcbd0655bc
Per Eriksson <per@interapp.se>
2021-08-28 14:32:09 +0200

    bug: forgot to (dis)connect

--- src/v1/index.js
+++ src/v1/index.js
@@ -5,34 +5,42 @@ const router = express.Router();
 
 router.get("/documents", async (req, res) => {
     const client = new DBClient();
+    await client.connect();
 
     const documents = await client.getDocuments();
+    await client.disconnect();
 
     res.json(documents);
 });
 
 router.get("/documents/:id", async (req, res) => {
     const client = new DBClient();
+    await client.connect();
 
     const document = await client.getDocument(req.params.id);
+    await client.disconnect();
 
     res.json(document);
 });
 
 router.put("/documents/:id", async (req, res) => {
     const client = new DBClient();
+    await client.connect();
 
     await client.updateDocument(req.params.id, req.body);
 
     const document = await client.getDocument(req.params.id);
+    await client.disconnect();
 
     res.json(document);
 });
 
 router.post("/documents", async (req, res) => {
     const client = new DBClient();
+    await client.connect();
 
     const document = await client.createDocument(req.body);
+    await client.disconnect();
 
     res.json(document);
 });
